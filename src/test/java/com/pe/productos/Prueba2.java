bc6a6e6256982223dbc43c99918e12bb6841e246
wilfredosystem <wilfredo.hr@gmail.com>
2023-12-06 15:48:01 -0500

    Update Prueba2.java
    modificado desde github

--- src/test/java/com/pe/productos/Prueba2.java
+++ src/test/java/com/pe/productos/Prueba2.java
@@ -3,12 +3,15 @@ package com.pe.productos;
 public class Prueba2 {
  String a;
  String b;
- int a1;
- int a2;
+ Integer a1;
+ float f1;
+ Integer a2;
 
  private void syso() {
 
-System.out.println("ok si va aca");
+System.out.println("modificado");
+  int total= a1+a2;
+  System.out.println("total:"+total);
 
 }
 }
