12d003e6d3806f47a5e91764e9ddaac3e998513a
whr <wilfredo.hr@gmail.com>
2023-12-14 18:22:44 -0500

    se comento codigo de sleep para manejo de timeout

--- src/main/java/com/pe/productos/controllers/ProductoController.java
+++ src/main/java/com/pe/productos/controllers/ProductoController.java
@@ -16,6 +16,9 @@ import com.pe.productos.models.service.IProductoService;
 @RestController
 public class ProductoController {
 	
+	@Autowired
+	private Environment env;
+	
 	@Value("${server.port}")
 	private Integer port;
 	
@@ -26,7 +29,8 @@ public class ProductoController {
 	public List<Producto> listar(){
 		return productoService.findAll().stream().map(//se agreago stream para agregar el puerto
 				producto->{
-					producto.setPort(port);
+					producto.setPort(port);//con port vemos el puerto que esta en properties, osea 0
+					producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));//con esto vemos el puerto real que se genera dinamicamente
 					return producto;
 				}).collect(Collectors.toList());
 	}
@@ -34,15 +38,25 @@ public class ProductoController {
 	@GetMapping("/ver/{id}")
 	public Producto detalle(@PathVariable Long id){
 		Producto producto = productoService.findById(id);
-		producto.setPort(port);//se agrego puerto
-	
-		/*	//hablitar para probar hystrix
+		producto.setPort(port);//se agrego puerto, con port vemos el puerto que esta en properties
+		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));//con esto vemos el puerto real que se genera dinamicamente
+		//hablitar para probar circuit breaker con hystrix
+		/*	
 		boolean ok=false;
 		if(!ok) {
 			throw new RuntimeException("no se pudo cargar el producto");
 		}
-		
 		*/
+		 
+		//para probar timeout con hystrix
+		
+//		try {
+//			Thread.sleep(3000L);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
 		return producto;
 	}
 }
